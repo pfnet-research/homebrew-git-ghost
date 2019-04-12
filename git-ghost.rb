9445d31865216767d6a7f8c333bdd6c6c7fbfaea
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-04-12 17:19:29 +0900

    Fix wrong sha256 hash

--- git-ghost.rb
+++ git-ghost.rb
@@ -2,7 +2,7 @@ class GitGhost < Formula
   desc "Synchronize your working directory efficiently to a remote place without committing the changes."
   homepage "https://github.com/pfnet-research/git-ghost"
   url "https://github.com/pfnet-research/git-ghost/releases/download/v0.1.1/git-ghost-darwin-amd64"
-  sha256 "990973e9b1df67010b4dca7f9b8c0ce0eedff6636ef975d57abe46f760bb2ab8"
+  sha256 "b4e54928fc3d3a84280e3386aa41e2d728b3bac6df7d81a4e67c5544e83c71a9"
   version "0.1.1"
 
 	bottle :unneeded
