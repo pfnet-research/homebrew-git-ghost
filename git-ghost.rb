798ceadfdca7ef6105d9bcd2c7f2f3b3c841f3c7
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-12-12 20:38:42 +0900

    Update to v0.1.4

--- git-ghost.rb
+++ git-ghost.rb
@@ -1,9 +1,9 @@
 class GitGhost < Formula
   desc "Synchronize your working directory efficiently to a remote place without committing the changes."
   homepage "https://github.com/pfnet-research/git-ghost"
-  url "https://github.com/pfnet-research/git-ghost/releases/download/v0.1.3/git-ghost-darwin-amd64"
-  sha256 "6e57bb127c2196f34ba48f708174d9a091c9a5b55068d2467283d4573a94c9f0"
-  version "0.1.3"
+  url "https://github.com/pfnet-research/git-ghost/releases/download/v0.1.4/git-ghost-darwin-amd64"
+  sha256 "2240cdaef660750e34830e8362dd594e5ecba9e8ad5f072bb4eea29442f172d8"
+  version "0.1.4"
 
 	bottle :unneeded
 
